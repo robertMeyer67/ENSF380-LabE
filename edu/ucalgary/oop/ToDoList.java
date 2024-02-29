1dc219a7d7413fbd520a6f533a54ccd4cbe88688
Robert Meyer <robert.meyer@ucalgary.ca>
2024-02-29 14:57:42 -0700

    Finished ToDoList

--- edu/ucalgary/oop/ToDoList.java
+++ edu/ucalgary/oop/ToDoList.java
@@ -1,5 +1,49 @@
 package edu.ucalgary.oop;
 
-public class ToDoList implements IToDoList{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
+public class ToDoList implements IToDoList{
+    private List<Task> tasks = new ArrayList<>();
+    private Stack<List<Task>> undoStack = new Stack<List<Task>>();
+    public void addTask(Task task) {
+        undoStack.push(tasks);
+        tasks.add(task);
+    }
+    public void completeTask(String id) {
+        undoStack.push(tasks);
+        for (Task task : tasks) {
+            if (task.getId() == id) {
+                task.setCompleted(true);
+            }
+        }
+    }
+    public void deleteTask(String id) {
+        undoStack.push(tasks);
+        tasks.removeIf(task -> task.getId() == id);
+    }
+    public void editTask(String id, String newTitle, boolean isCompleted) {
+        undoStack.push(tasks);
+        for (Task task : tasks) {
+            if (task.getId() == id) {
+                task.setTitle(newTitle);
+                task.setCompleted(isCompleted);
+            }
+        }
+    }
+    public void undo() {
+        tasks = undoStack.pop();
+    }
+    public List<Task> listTasks() {
+//        for (Task task : tasks) {
+//            if (task.isCompleted()) {
+//                System.out.printf("%s10 Complete", task.getTitle());
+//            }
+//            else {
+//                System.out.printf("%s10 Incomplete", task.getTitle());
+//            }
+//        }
+        return tasks;
+    }
 }
