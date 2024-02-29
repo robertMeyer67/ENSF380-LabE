b02c2440eb1b2de8c8a8b322f1c2264cfefdd338
Junaid Khan <junaid.khan1@ucalgary.ca>
2024-02-29 14:33:17 -0700

    Completed

--- edu/ucalgary/oop/Task.java
+++ edu/ucalgary/oop/Task.java
@@ -3,6 +3,47 @@ package edu.ucalgary.oop;
 import java.util.Objects;
 
 public class Task {
+    // Properties
+    private String id;
+    private String title;
+    private boolean isCompleted;
+
+    // Constructor
+    public Task(String id, String title, boolean isCompleted) {
+        this.id = id;
+        this.title = title;
+        this.isCompleted = isCompleted;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    // Copy method for deep copying task objects
+    public Task copy() {
+        return new Task(this.id, this.title, this.isCompleted);
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -11,7 +52,7 @@ public class Task {
         Task task = (Task) obj;
         return Objects.equals(id, task.id) &&
                Objects.equals(title, task.title) &&
-               isCompleted == task.isCompleted; 
+               isCompleted == task.isCompleted;
     }
 
     @Override
