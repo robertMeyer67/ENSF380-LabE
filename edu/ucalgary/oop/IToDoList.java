c0aa80f5be4cc79a3eaa937ae68dba53c7686c0f
Taiwu <chen.taiwu@ucalgary.ca>
2024-02-29 14:32:51 -0700

    Update IToDoList.java

--- edu/ucalgary/oop/IToDoList.java
+++ edu/ucalgary/oop/IToDoList.java
@@ -1,5 +1,14 @@
-class Untitled {
-	public static void main(String[] args) {
-		
-	}
+package edu.ucalgary.oop;
+
+import java.util.*;
+
+interface IToDoList{
+	void addTask(Task task);
+	void completeTask(String id);
+	void deleteTask(String id);
+	void editTask(String id, String newTitle, boolean isCompleted);
+	void undoAddTask();
+	void undoDeleteTask();
+	void multipleUndos();
+	List<Task> listTasks();
 }
